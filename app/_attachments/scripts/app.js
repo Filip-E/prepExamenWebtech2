6f9d47fa600330e4688a731a1fd49502ccf4765b
FilipElen <filipelengit@outlook.com>
2017-11-06 14:37:13 +0100

    show persons from couchdb and started html to add person

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -8,21 +8,43 @@ angular.module('prepApp', ['ngRoute'])
                 templateUrl: 'assets/home.html',
                 controller: 'homeCtrl'
             })
+            .when('/newPerson.html', {
+                templateUrl: 'assets/newPerson.html',
+                controller: 'homeCtrl'
+            })
             .otherwise({
                 redirectTo: '/home'
             });
     })
-    .controller('homeCtrl', function ($scope, addressServ) {
-    	$scope.names = 
-    	[{
-    			'Name' : 'Filip',
-    			'Country' : 'Belgium'
-    				
-    	},{
-    		'Name' : 'Fili',
-			'Country' : 'Belgiu'
-    	}];
-    })
-    .service('addressServ',function($http){
+    .controller('homeCtrl', function ($scope,$window, personSrv) {
+    	$('#newPerson').on('click',function(){
+    		$window.location.href = '/newPerson.html';
+    	};
+    	personSrv.getPersons().then(function(data){
+    		console.log('resolve from ctrl: ' + data.toString());
+    		$scope.persons = data;
+    	},function(err){
+    		console.log('ctrl promise error: ' + err);
+    	});
     	
+    })
+    .service('personSrv',function($http, $q){
+    	var ALL_DOCS = '../../_all_docs?include_docs=true';
+    	this.getPersons = function(){
+    		var q = $q.defer();
+
+        	$http.get(ALL_DOCS)
+        		.then(function(data){
+        			var arr = data.data.rows;
+        			for(var i = 0; i < arr.length; i++){
+        				if(arr[i].id == "_design/app"){
+        					arr.splice(i,1);
+        				}
+        			};
+        			q.resolve(arr);
+        		},function(err){
+        			q.reject(err);
+        		});
+        	return q.promise;
+    	};
     });
